3a82c9b3d649bc56b964281843ff104ebf574734
marcomonto <marcomontorsi97@gmail.com>
2025-07-27 17:15:31 +0200

    fix: run tests via docker

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -11,15 +11,22 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        //web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        //commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        $dontLog = [
+            InvalidArgumentException::class,
+            NotFoundHttpException::class,
+            MethodNotAllowedHttpException::class,
+            ValidationException::class,
+        ];
+        $exceptions->dontReport($dontLog);
         $exceptions->render(function (NotFoundHttpException $e, Request $request) {
             return response()->json([
                 'message' => 'Route not found',
@@ -52,7 +59,6 @@ return Application::configure(basePath: dirname(__DIR__))
         });
 
         $exceptions->render(function (\Exception $e, Request $request) {
-            Log::error($e->getMessage());
             return response()->json([
                 'message' => 'Internal server error',
             ], 500);
